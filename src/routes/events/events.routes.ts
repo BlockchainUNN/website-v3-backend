3760932389f345eb58536ccf929067e24e7b84f4
emmanueldindu <emmanuelnwobodoc04@gmail.com>
2024-09-19 18:16:34 +0100

    post event

--- src/routes/events/events.routes.ts
+++ src/routes/events/events.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import eventsControllers from "../../controllers/events/events.controllers";
+import AuthMiddleware from "../../middlewares/auth.middleware";
+import { permissionsCheck } from "../../middlewares/permissions.middleware";
+import { upload } from "../../config/upload";
 
 
 const getEventsRoutes = Router();
@@ -7,5 +10,17 @@ const getEventsRoutes = Router();
 // Public route to get events
 getEventsRoutes.get("/events", eventsControllers.getEvents);
 getEventsRoutes.get("/events/:id", eventsControllers.getEventDetails)
+getEventsRoutes.post(
+    "/events",
+    AuthMiddleware.protectRoute,
+    permissionsCheck({ role: ["admin", "event_admin", "superadmin"] }),
+    upload.single("cover_image"), // Multer middleware to handle single file upload
+    eventsControllers.createEvent // Event creation controller
+  );
+
+
 export default getEventsRoutes;
+
+
+
 
