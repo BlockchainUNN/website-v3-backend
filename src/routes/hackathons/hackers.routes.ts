be8b09df69e5f52b6c4f06ef1a3bf4099c968c91
Peter <obidaberechukwu@gmail.com>
2024-10-03 22:44:10 +0100

    working on hackerton registeration

--- src/routes/hackathons/hackers.routes.ts
+++ src/routes/hackathons/hackers.routes.ts
@@ -4,5 +4,7 @@ import hackerControllers from "../../controllers/hackathons/hackers.controllers"
 const hackersRoutes = Router();
 
 hackersRoutes.post("/hackers/:id", hackerControllers.create);
+hackersRoutes.get("/hackers/:id/:email", hackerControllers.getHacker);
+hackersRoutes.post("/hackers/login/:id", hackerControllers.login);
 
 export default hackersRoutes;
