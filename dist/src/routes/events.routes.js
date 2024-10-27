50cb6689aac3110cd94a059e417293608abe9186
Peter <obidaberechukwu@gmail.com>
2024-10-27 12:43:58 +0100

    fixed route

--- dist/src/routes/events.routes.js
+++ dist/src/routes/events.routes.js
@@ -9,6 +9,6 @@ const eventsRoutes = express_1.default.Router();
 // Basic Events Routes
 eventsRoutes.post("/events/registeration/:id", events_controllers_1.default.register);
 eventsRoutes.post("/events/attendees/:id", events_controllers_1.default.getAttendee);
-eventsRoutes.get("/events/attendees/:id", events_controllers_1.default.downloadAttendee);
+eventsRoutes.get("/events/attendees/:id/download-cvs", events_controllers_1.default.downloadAttendee);
 eventsRoutes.get("/events/attendees/count/:id", events_controllers_1.default.getAttendeeCount);
 exports.default = eventsRoutes;
