c69a567c6255c51db788efb084febdb1248d78e5
root <root@vps.server.local>
2024-10-31 05:56:59 +0000

    prisma changes

---
+++ prisma/migrations/20241031055609_adding_otp_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "OTP" (
+    "id" TEXT NOT NULL,
+    "otpCode" TEXT NOT NULL,
+    "expiration" TIMESTAMP(3) NOT NULL,
+    "isUsed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "OTP_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "OTP" ADD CONSTRAINT "OTP_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
